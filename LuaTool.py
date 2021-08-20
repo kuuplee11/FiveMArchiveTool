057c1dc92cd437af194ce40a6b9cbd64004d8960
kuuplee11 <82771602+kuuplee11@users.noreply.github.com>
2021-08-20 20:22:11 +0200

    Add files via upload

--- LuaTool.py
+++ LuaTool.py
@@ -17,31 +17,37 @@ $$ | \$$\ \$$$$$$  |$$ |      $$$$$$$$\ $$$$$$$$\                        $$ |   
     parent_dir = input("Pon el directorio donde quires crear la carpeta: ")
     directory = input("Pon el nombre del script: ")
 
-    cl = 'Client'
-    sv = 'Server'
-    
-    # mainsv = open('main.lua', 'w')
-    # maincl = open('main.lua', 'w')
-    # configlua = open('config.lua', 'w')
-    # mainfestlua = open('fxmainfest.lua', 'w')
+    mainsv = "main.lua"
+    maincl = "main.lua"
+    configlua = "config.lua"
+    mainfestlua = "fxmainfest.lua"
 
     path = os.path.join(parent_dir, directory)
-    clside = os.path.join(parent_dir, directory, cl)
-    svside = os.path.join(parent_dir, directory, sv)
-
-    # svsidelua = os.path.join(parent_dir, directory, sv, mainsv)
-    # clsidelua = os.path.join(parent_dir, directory, cl, maincl)
-    # config = os.path.join(parent_dir, directory, configlua)
-    # mainfest = os.path.join(parent_dir, directory, mainfestlua)
+    clside = os.path.join(parent_dir, directory, "Client")
+    svside = os.path.join(parent_dir, directory, "Server")
+    svsidelua = os.path.join(parent_dir, directory, "Server", mainsv)
+    clsidelua = os.path.join(parent_dir, directory, "Client", maincl)
+    config = os.path.join(parent_dir, directory, configlua)
+    mainfest = os.path.join(parent_dir, directory, mainfestlua)
 
     try:
         os.mkdir(path)
         os.mkdir(clside)
         os.mkdir(svside)
-        # os.mkdir(svsidelua)
-        # os.mkdir(clsidelua)
-        # os.mkdir(config)
-        # os.mkdir(mainfest)
+        fsv = open(svsidelua, 'w')
+        fsv.write("-- Archivo Server.lua Creado Por La KUPLE-TOOL")    
+        fsv.close()
+        fcl = open(clsidelua, 'w')
+        fcl.write("-- Archivo Client.lua Creado Por La KUPLE-TOOL")
+        fcl.close()
+        fcf = open(config, 'w')
+        fcf.write("-- Archivo Config.lua Creado Por La KUPLE-TOOL")
+        fcf.close()
+        fm = open(mainfest, 'w')
+        fm.write("-- Archivo FxMainfest.lua Creado Por La KUPLE-TOOL")
+        fm.close()
+
+
 
         print("Directory " , directory ,  " Created ") 
     except FileExistsError:
